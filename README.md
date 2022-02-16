7c7fb4648784597e6c3f0158d18f1095bdde6eaa
Nicolas Machado <82627291+nicolasmmachado@users.noreply.github.com>
2022-02-15 21:44:26 -0300

    Finalização Projeto

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # desafio-EQI
  
+Desafio proposto para a vaga de desenvolvedor front-end, feito com HTML, CSS e JS puro.
+O resultado final é uma página que busca dados na API-fake fornecida, preenchendo logo no carregamento da página os dados de IPCA e CDI.
+
+Para o mesmo funcionar, execute no projeto "npm install" e "npx json-server db.json", logo após isso é só abrir pelo index e o projeto funcionará normalmente.
+
+Como foi proposto todos os campos tem uma checagem por digitação, é necessário escolher tanto o rendimento quanto a indexação, caso algum dos dois falte um alert ocorrerá.
+
+Todos os arquivos tirando a API se encontram na pasta src.
--- src/js/script.js
+++ src/js/script.js
@@ -1,44 +1,51 @@
-async = () => {
+async function loadPage() {
     let loadPage = await fetch("http://localhost:3000/indicadores", { 
         method: "GET"
     });
     let loadPageJson = await loadPage.json();
-    document.querySelector(".income-ipca").value = `${loadPageJson[0].valor}%`;
-    document.querySelector(".contribution-cdi").value = `${loadPageJson[1].valor}%`;
-    let loadSimulation = await fetch("http://localhost:3000/simulacoes", { 
+    document.querySelector(".income-ipca").value = `${loadPageJson[1].valor}%`;
+    document.querySelector(".contribution-cdi").value = `${loadPageJson[0].valor}%`;
+    let loadSimulation = await fetch("http://localhost:3000/simulacoes?tipoIndexacao=pre&tipoRendimento=bruto", { 
         method: "GET"
     });
     let loadSimulationJson = await loadSimulation.json();
     console.log(loadSimulationJson);
 }
 
+loadPage();
+
 const brute = document.querySelector(".brute-btn");
 const liquid = document.querySelector(".liquid-btn");
 const pre = document.querySelector(".pre-btn");
 const pos = document.querySelector(".pos-btn");
 const fixed = document.querySelector(".fixed-btn");
-const show = document.querySelectorAll(".show");
+const hide = document.querySelectorAll(".hide");
 const incomeInput = document.querySelector(".income-input");
 const incomeTime = document.querySelector(".income-time");
 const contributionInput = document.querySelector(".contribution-input");
 const contributionProf = document.querySelector(".contribution-profitability");
 const hr = document.querySelectorAll("hr");
 const label = document.querySelectorAll("label");
+const simulate = document.querySelector(".simulate-btn");
+let income = "";
+let contribution = "";
 
 brute.addEventListener("click", () => {
     brute.classList.remove("checked");
     liquid.classList.remove("checked");
-    show[1].classList.add("show");
+    hide[1].classList.add("hide");
     brute.classList.add("checked");
-    show[0].classList.remove("show");
+    hide[0].classList.remove("hide");
+    income = "bruto";
 });
 
 liquid.addEventListener("click", () => {
     brute.classList.remove("checked");
     liquid.classList.remove("checked");
-    show[0].classList.add("show");
+    hide[0].classList.add("hide");
     liquid.classList.add("checked");
-    show[1].classList.remove("show");
+    hide[1].classList.remove("hide");
+    income = "liquido"
 });
 
 incomeInput.addEventListener("keyup", () => {
@@ -48,15 +55,15 @@ incomeInput.addEventListener("keyup", () => {
         incomeInput.previousSibling.classList.add("warning");
         label[1].classList.add("warning");
         hr[0].classList.add("warning");
-        show[2].classList.remove("show");
-        show[2].classList.add("warning");
+        hide[2].classList.remove("hide");
+        hide[2].classList.add("warning");
     }
     else{
         incomeInput.previousSibling.classList.remove("warning");
         label[1].classList.remove("warning");
         hr[0].classList.remove("warning");
-        show[2].classList.add("show");
-        show[2].classList.remove("warning");
+        hide[2].classList.add("hide");
+        hide[2].classList.remove("warning");
     }
 });
 
@@ -65,13 +72,13 @@ incomeTime.addEventListener("keyup", () => {
 
     if(isNaN(testNum)) {
         label[2].classList.add("warning");
-        show[3].classList.remove("show");
-        show[3].classList.add("warning");
+        hide[3].classList.remove("hide");
+        hide[3].classList.add("warning");
     }
     else{
         label[2].classList.remove("warning");
-        show[3].classList.add("show");
-        show[3].classList.remove("warning");
+        hide[3].classList.add("hide");
+        hide[3].classList.remove("warning");
     }
 });
 
@@ -79,30 +86,33 @@ pre.addEventListener("click", () => {
     pre.classList.remove("checked");
     pos.classList.remove("checked");
     fixed.classList.remove("checked");
-    show[5].classList.add("show");
-    show[6].classList.add("show");
+    hide[5].classList.add("hide");
+    hide[6].classList.add("hide");
     pre.classList.add("checked");
-    show[4].classList.remove("show");
+    hide[4].classList.remove("hide");
+    contribution = "pre";
 })
 
 pos.addEventListener("click", () =>{
     pre.classList.remove("checked");
     pos.classList.remove("checked");
     fixed.classList.remove("checked");
-    show[4].classList.add("show");
-    show[6].classList.add("show");
+    hide[4].classList.add("hide");
+    hide[6].classList.add("hide");
     pos.classList.add("checked");
-    show[5].classList.remove("show");
+    hide[5].classList.remove("hide");
+    contribution = "pos";
 });
 
 fixed.addEventListener("click", () =>{
     pre.classList.remove("checked");
     pos.classList.remove("checked");
     fixed.classList.remove("checked");
-    show[4].classList.add("show");
-    show[5].classList.add("show");
+    hide[4].classList.add("hide");
+    hide[5].classList.add("hide");
     fixed.classList.add("checked");
-    show[6].classList.remove("show");
+    hide[6].classList.remove("hide");
+    contribution = "ipca";
 });
 
 contributionInput.addEventListener("keyup", () => {
@@ -113,15 +123,15 @@ contributionInput.addEventListener("keyup", () => {
         contributionInput.previousSibling.classList.add("warning");
         label[5].classList.add("warning");
         hr[1].classList.add("warning");
-        show[7].classList.remove("show");
-        show[7].classList.add("warning");
+        hide[7].classList.remove("hide");
+        hide[7].classList.add("warning");
     }
     else{
         contributionInput.previousSibling.classList.remove("warning");
         label[5].classList.remove("warning");
         hr[1].classList.remove("warning");
-        show[7].classList.add("show");
-        show[7].classList.remove("warning");
+        hide[7].classList.add("hide");
+        hide[7].classList.remove("warning");
     }
 });
 
@@ -130,13 +140,13 @@ contributionProf.addEventListener("keyup", () => {
 
     if(isNaN(testNum)) {
         label[6].classList.add("warning");
-        show[8].classList.remove("show");
-        show[8].classList.add("warning");
+        hide[8].classList.remove("hide");
+        hide[8].classList.add("warning");
     }
     else{
         label[6].classList.remove("warning");
-        show[8].classList.add("show");
-        show[8].classList.remove("warning");
+        hide[8].classList.add("hide");
+        hide[8].classList.remove("warning");
     }
 });
 
@@ -150,9 +160,36 @@ document.querySelector(".clear-btn").addEventListener("click", () => {
     pre.classList.remove("checked");
     pos.classList.remove("checked");
     fixed.classList.remove("checked");
-    show[0].classList.add("show");
-    show[1].classList.add("show");
-    show[4].classList.add("show");
-    show[5].classList.add("show");
-    show[6].classList.add("show");
+    hide[0].classList.add("hide");
+    hide[1].classList.add("hide");
+    hide[4].classList.add("hide");
+    hide[5].classList.add("hide");
+    hide[6].classList.add("hide");
+    document.querySelector(".information").classList.add("hide");
 });
+
+simulate.addEventListener("click", loadSimulation);
+
+async function loadSimulation(){
+    if((brute.classList.contains("checked") || liquid.classList.contains("checked")) && (pre.classList.contains("checked") || pos.classList.contains("checked") || fixed.classList.contains("checked"))){
+        
+        let loadSimulation = await fetch(`http://localhost:3000/simulacoes?tipoIndexacao=${contribution}&tipoRendimento=${income}`, { 
+        method: "GET"
+        });
+        let loadSimulationJson = await loadSimulation.json();
+        const info = document.querySelectorAll(".info p");
+        console.log(loadSimulationJson);
+        console.log(info);
+        info[0].innerHTML = "R$ " + loadSimulationJson[0].valorFinalBruto;
+        info[1].innerHTML = loadSimulationJson[0].aliquotaIR + "%";
+        info[2].innerHTML = "R$ " + loadSimulationJson[0].valorPagoIR;
+        info[3].innerHTML = "R$" + loadSimulationJson[0].valorFinalLiquido;
+        info[4].innerHTML = "R$ " + loadSimulationJson[0].valorTotalInvestido;
+        info[5].innerHTML = "R$ " + loadSimulationJson[0].ganhoLiquido;
+        document.querySelector(".information").classList.remove("hide");
+
+    }
+    else{
+        alert("Favor escolher o Rendimento e Indexação.")
+    }
+}
